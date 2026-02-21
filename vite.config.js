63f9c9455293b99eebe7a70921f7484839e4da51
ronnelb <barasharironnel29@gmail.com>
2026-02-21 15:17:01 +0800

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [reactRouter(), tailwindcss()],
+  plugins: [reactRouter(), tailwindcss(), netlifyPlugin()],
 });
