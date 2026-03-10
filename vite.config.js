79af67aeb851e99af0e30bb4522205f65a209cb8
ronnelb <barasharironnel29@gmail.com>
2026-03-10 11:56:31 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { reactRouter } from "@react-router/dev/vite";
 import { vercelPreset } from "@vercel/react-router/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
+  root: path.resolve(__dirname),
   plugins: [
     tailwindcss(),
     reactRouter({ presets: [vercelPreset()] }),
